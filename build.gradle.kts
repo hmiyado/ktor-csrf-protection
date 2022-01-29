75f51beb2b2a207a4a90eb661c6714a1fbb9df2f
miyado <hmiyado@gmail.com>
2022-01-29 13:15:46 +0900

    enable to publish artifact

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,19 @@
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
 
 plugins {
     kotlin("jvm") version "1.6.10"
+    `maven-publish`
+    signing
 }
 
-group = "com.github.hmiyado"
-version = "0.1"
+group = "io.github.hmiyado"
+version = "0.1-SNAPSHOT"
+
+val localProperties = Properties().apply {
+    load(FileInputStream(File(rootProject.rootDir, "local.properties")))
+}
 
 repositories {
     mavenCentral()
@@ -20,6 +29,23 @@ tasks {
     test {
         useJUnitPlatform()
     }
+
+    val sourcesJar by creating(Jar::class) {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+    }
+
+    val javadocJar by creating(Jar::class) {
+        dependsOn.add(javadoc)
+        archiveClassifier.set("javadoc")
+        from(javadoc)
+    }
+
+    artifacts {
+        archives(sourcesJar)
+        archives(javadocJar)
+        archives(jar)
+    }
 }
 
 dependencies {
@@ -32,5 +58,50 @@ dependencies {
     testImplementation("io.mockk:mockk:1.12.1")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest.extensions:kotest-assertions-ktor:$ktorAssertionVersion")
+}
 
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "ktor-csrf-protection"
+
+            from(components["java"])
+
+            pom {
+                name.set("Ktor Csrf Protection")
+                description.set("Protect Csrf in Ktor server")
+                url.set("https://github.com/hmiyado/ktor-csrf-protection")
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("hmiyado")
+                        name.set("hmiyado")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/hmiyado/ktor-csrf-protection.git")
+                }
+            }
+            repositories {
+                maven {
+                    val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+                    url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+                    credentials {
+                        username = localProperties["ossrhUsername"].toString()
+                        password = localProperties["ossrhPassword"].toString()
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
